1d5917f718a88d16e75e6e113db8d8e1d13e4635
shivam-0233 <shivam@ineuron.ai>
2023-06-22 08:43:57 +0530

    fix: footer complete, fixed metadata

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const date = new Date();
@@ -6,21 +8,19 @@ const Footer = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-          </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
-        </a>
+        <Link
+          href={'/'}
+          className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+        >
+          <Image
+            src={'/ammajaan-logo.svg'}
+            alt="AmmaJaan"
+            width="20"
+            height="20"
+            className="mr-2"
+          />
+          <span className="ml-3 text-xl">AmmaJaan</span>
+        </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © {date.getFullYear()} AmmaJaan —
           <a
@@ -33,7 +33,11 @@ const Footer = () => {
           </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-500">
+          <a
+            target="_blank"
+            href="https://facebook.com/shivamvijaywargi"
+            className="text-gray-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -45,7 +49,11 @@ const Footer = () => {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a
+            target="_blank"
+            href="https://twitter.com/shivamvijaywarg"
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -57,7 +65,11 @@ const Footer = () => {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a
+            target="_blank"
+            href="https://instagram.com/shivamvijaywargi"
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="none"
               stroke="currentColor"
@@ -71,7 +83,11 @@ const Footer = () => {
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a
+            target="_blank"
+            href="https://linkedin.com/in/shivamvijaywargi"
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="currentColor"
               stroke="currentColor"
