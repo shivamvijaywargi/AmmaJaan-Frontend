82fcd5a819348bf92797b0362b8ceb94a1ab19b3
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-07-23 12:41:11 +0530

    feat: add banner image on homepage

--- src/app/(main-site)/page.tsx
+++ src/app/(main-site)/page.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image';
+import Banner from 'public/banner.png';
+
 export default function Home() {
   return (
     <section className="px-2 md:px-5 mx-auto max-w-7xl">
-      Welcome to AmmaJaan
+      {/* Banner image */}
+      <Image src={Banner} alt="AmmaJaan Banner" />
+
+      {/* Products carousel */}
+
+      {/* Featured Product */}
     </section>
   );
 }
